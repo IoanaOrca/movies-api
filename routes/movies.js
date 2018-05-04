b7c5207038267ec73fd3c6659b7dbc608070f89e
Ioana Iordache <ioanaorka@gmail.com>
2018-05-04 18:28:29 +0200

    form

--- routes/movies.js
+++ routes/movies.js
@@ -13,4 +13,30 @@ router.get('/', (req, res, next) => {
   .catch(next)
 });
 
+router.post('/', (req, res, next) => {
+
+  const title = req.body.title;
+  const year = Number(req.body.year);
+  const posterUrl = req.body.posterUrl;
+
+  if (!title || !year || !posterUrl){
+    return res.status(422).json({code: 'unprocessable-entity'});
+  }
+  const newMovie = new Movie({title, year, posterUrl});
+
+  newMovie.save()
+  .then((result) => {
+    res.status(201).json({code: 'okey'})
+  })
+  .catch(next);
+});
+
+router.get('/:id', (req, res, next) => {
+  Movie.findById (req.params.id)
+  .then((result) => {
+    res.json(result)
+  })
+  .catch(next)
+});
+
 module.exports = router;
